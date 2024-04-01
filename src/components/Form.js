bdb849a7364edeec65dfa5f7c566b559461b5204
Quwanish <quwanishmi@gmail.com>
2024-04-01 14:38:50 +0500

    first

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import "./Form.css"
+
+function Form() {
+  return (
+    <>
+<label className="switch">
+        <input type="checkbox" className="switch--input" />
+        <span className="switch--slider" ></span>
+    </label>      
+      
+    </>
+  );
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Form from "./components/Form"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Form/>
   </React.StrictMode>
 );
 
@@ -15,3 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
